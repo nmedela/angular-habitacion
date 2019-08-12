fd65defd8370dc6a924c681f276200a08028dc7b
nmedela <medela.nicolas@gmail.com>
2019-08-11 22:07:10 -0300

    Se cambia ruta y id para modificar intensidad de luz

--- src/app/servicios/config.ts
+++ src/app/servicios/config.ts
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://192.168.0.99:9000';
-export const API_URL = 'http://localhost:4000';
+export const API_URL = 'http://192.168.0.99:4000';
 // export const API_URL = 'http://192.168.0.11:8080';
